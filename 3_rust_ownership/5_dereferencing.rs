30357c9cdd8899a4f9e6a97508fedcf4ab89b378
jojostx <ikuskid7@gmail.com>
2025-09-19 19:20:40 +0100

    dereferencing

--- 3_rust_ownership/5_dereferencing.rs
+++ 3_rust_ownership/5_dereferencing.rs
@@ -9,11 +9,12 @@ fn main() {
     println!("some_data is: {some_data}, deref_copy is: {deref_copy}");
 
     let mut heap_data = vec![5, 6, 7];
-    let ref_1 = &heap_data;
-    let ref_2 = ref_1;
-    let ref_3 = ref_1;
-    let deref_copy = ref_1.clone();
+    let ref_1 = &mut heap_data;
+    // let ref_2 = *ref_1; // problenatic code
+    let ref_2 = ref_1.clone();
+    println!("ref_2: {:?}", ref_2);
 
-    let move_out = ref_1;
-    // let move_out_again = ref_1;
+    let move_out = ref_1; // a move occurs here
+    // let move_out_again = ref_1; // so another move fails
+    println!("move_out: {:?}", move_out);
 }
