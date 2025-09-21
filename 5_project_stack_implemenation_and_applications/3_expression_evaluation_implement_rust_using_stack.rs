19cefd06e850c49e433f55c7c0990e3022c49a20
jojostx <ikuskid7@gmail.com>
2025-09-21 17:58:46 +0100

    incorrect impl

--- 5_project_stack_implemenation_and_applications/3_expression_evaluation_implement_rust_using_stack.rs
+++ 5_project_stack_implemenation_and_applications/3_expression_evaluation_implement_rust_using_stack.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 fn new_stack(maxsize: usize) -> Vec<String> {
     Vec::with_capacity(maxsize)
 }
@@ -24,7 +26,21 @@ fn size(stack: &[String]) -> usize {
 }
 
 fn is_operator(ch: char) -> bool {
-    ['+', '-', '*', '/', '(', ')'].contains(&ch)
+    for ch_ in ['+', '-', '*', '/', '(', ')'].into_iter() {
+        if ch_ == ch {
+            return true;
+        }
+    }
+
+    false
+}
+fn get_priority(op: &String) -> u8 {
+    let priority_op: HashMap<&'static str, u8> =
+        HashMap::from([("-", 1), ("+", 2), ("*", 3), ("/", 4), ("^", 5)]);
+
+    let priority = priority_op.get(op.as_str()).unwrap_or(&0);
+
+    *priority
 }
 
 fn individual_symbols(expr: String) -> Vec<String> {
@@ -52,12 +68,65 @@ fn individual_symbols(expr: String) -> Vec<String> {
     tokenized_ops
 }
 
+fn build_postfix(tokens: Vec<String>) -> Vec<String> {
+    /*
+    1. if operator <= to current operator in priority in the stack, pop items into the postfix list until otherwise
+    2. if character is ( push to stack
+    3. if character is ) pop items from stack to postfix list until you encounter (
+    4. if character is an operand, push to postfix list
+     */
+    // - + * / ^
+    let stack_size = tokens.len();
+    let mut stack = new_stack(stack_size);
+    let mut postfix_vec: Vec<String> = Vec::new();
+
+    for ch in tokens {
+        match ch.as_str() {
+            "+" | "-" | "*" | "/" | "^" => {
+                if !stack.is_empty() {
+                    if get_priority(&ch) <= get_priority(stack.last().unwrap()) {
+                        while get_priority(&ch) <= get_priority(stack.last().unwrap()) {
+                            // keep popping from stack and pushing into the postfix
+                            postfix_vec.push(pop(&mut stack).unwrap());
+                        }
+
+                        // push to the stack
+                        push(&mut stack, ch, stack_size);
+                    } else {
+                        push(&mut stack, ch, stack_size);
+                    }
+                } else {
+                    push(&mut stack, ch, stack_size);
+                }
+            }
+            "(" => {
+                push(&mut stack, ch, stack_size);
+            }
+            ")" => {
+                while stack.last().unwrap() != "(" {
+                    // keep popping from stack and pushing into the postfix
+                    postfix_vec.push(pop(&mut stack).unwrap());
+                }
+                pop(&mut stack).unwrap();
+            }
+            _ => postfix_vec.push(ch),
+        }
+    }
+
+    while !stack.is_empty() {
+        postfix_vec.push(pop(&mut stack).unwrap());
+    }
+
+    postfix_vec
+}
+
 fn main() {
     let input_string = String::from("(2 + 55) - 36");
-    let tokenized_expr = individual_symbols(input_string);
+    let tokens = individual_symbols(input_string);
 
-    // let maxsize = input_string.len();
-    // let mut stack = new_stack(maxsize);
+    println!("{:?}", tokens);
 
-    println!("{:?}", tokenized_expr);
+    let postfix = build_postfix(tokens);
+
+    println!("{:?}", postfix);
 }
