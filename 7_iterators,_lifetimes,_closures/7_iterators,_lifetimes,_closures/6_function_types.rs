3b863c35861e886ded53e71bb9afc916a61fba75
jojostx <ikuskid7@gmail.com>
2025-10-19 19:44:08 +0100

    function types

--- 7_iterators,_lifetimes,_closures/7_iterators,_lifetimes,_closures/6_function_types.rs
+++ 7_iterators,_lifetimes,_closures/7_iterators,_lifetimes,_closures/6_function_types.rs
@@ -1,51 +1,25 @@
-   // -------------------------------------------
-   // 			Function Types    
-   //           	- Basic syntax and use
-   //           	- Function types as parameters to function
-   // -------------------------------------------
+// -------------------------------------------
+// 			Function Types
+//           	- Basic syntax and use
+//           	- Function types as parameters to function
+// -------------------------------------------
 
-/*
 fn max(x: i32, y: i32) -> i32 {
-    if x > y {
-        x
-    } else {
-        y
-    }
+    if x > y { x } else { y }
 }
 
 fn min(x: i32, y: i32) -> i32 {
-    if x < y {
-        x
-    } else {
-        y
-    }
+    if x < y { x } else { y }
 }
-
-fn main(){
-let mut f = max;
-println!("The minimum of the two values is {}",f(2,3));
-}
-*/
-
-
-/*
 
 fn prints_name(name: &str) {
-    print!("The name is {}",name); 
+    print!("The name is {}", name);
 }
 
 fn prints_full_info(f: fn(&str), some_one: &str, age: i32) {
-    f(some_one); 
-	println!(" and my age is {}", age);
+    f(some_one);
+    println!(" and my age is {}", age);
 }
-
-fn main() {
-	let (my_name, my_age) = (String::from("Nouman"), 40); 
-	prints_full_info(prints_name, &my_name, my_age) ;    	
-}
-*/ 
-
-
 
 fn add_one(x: i32) -> i32 {
     x + 1
@@ -56,6 +30,14 @@ fn do_twice(f: fn(i32) -> i32, arg: i32) -> i32 {
 }
 
 fn main() {
+    let f = max;
+    let f2 = min;
+    println!("The minimum of the two values is {}", f(2, 3));
+    println!("The minimum of the two values is {}", f2(2, 3));
+
+    let (my_name, my_age) = (String::from("Nouman"), 40);
+    prints_full_info(prints_name, &my_name, my_age);
+
     let answer = do_twice(add_one, 5);
     println!("The answer is: {}", answer);
-} 
+}
