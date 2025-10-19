05fcca17dd31815f5b3d4db3e339b5a58fe399c3
jojostx <ikuskid7@gmail.com>
2025-10-19 18:47:51 +0100

    eliding lifetimes

--- 7_iterators,_lifetimes,_closures/7_iterators,_lifetimes,_closures/3_eliding_lifetimes.rs
+++ 7_iterators,_lifetimes,_closures/7_iterators,_lifetimes,_closures/3_eliding_lifetimes.rs
@@ -10,30 +10,36 @@
    the lifetime of self is assigned to all output lifetime parameters.
 */
 
-//Example 1:
-fn main() {
-    let str_1 = "some str";
-
-    let recieved_str = return_str(&str_1);
-}
 // Code with Lifetime Elision
-// fn return_str(s_1: &str) -> &str {
-//     s_1
-// }
+fn return_str(s_1: &str) -> &str {
+    s_1
+}
 
 // Code without Lifetime Elision
-fn return_str<'a>(s_1: &'a str) -> &'a str {
+fn return_str2<'a>(s_1: &'a str) -> &'a str {
     s_1
 }
 
 // Example 2:
-/* fn main() {
-    let str_1 = "some str";
-    let str_2 = "other str";
-    let recieved_str = return_str(&str_1, &str_2);
-}
 
-fn return_str<'a, 'b>(s_1: &'a str, s_2: &'b str) -> &'a str {
+fn return_str3<'a, 'b>(s_1: &'a str, s_2: &'b str) -> &'a str {
     s_1
 }
-*/
+
+fn main() {
+    let str_1 = "some str";
+
+    let recieved_str = return_str(&str_1);
+
+    println!("{} has a length of {}", recieved_str, recieved_str.len());
+
+    let recieved_str2 = return_str2(&str_1);
+
+    println!("{} has a length of {}", recieved_str2, recieved_str2.len());
+
+    let str_1 = "some str";
+    let str_2 = "other str";
+    let recieved_str = return_str3(&str_1, &str_2);
+
+    println!("{} has a length of {}", recieved_str, recieved_str.len());
+}
