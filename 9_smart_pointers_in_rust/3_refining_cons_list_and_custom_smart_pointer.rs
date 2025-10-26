b6b6e20e3b95ce619dae251eee8111a48b368371
jojostx <ikuskid7@gmail.com>
2025-10-26 12:09:26 +0100

    custom smart pointer with implementation for deref and drop

--- 9_smart_pointers_in_rust/3_refining_cons_list_and_custom_smart_pointer.rs
+++ 9_smart_pointers_in_rust/3_refining_cons_list_and_custom_smart_pointer.rs
@@ -1,123 +1,60 @@
-    // -------------------------------------------
-    // 			Smart Pointers
-    //          	- Box Pointers 
-    //          	- Issue with Box Pointer implementation of Cons variant in List
-    // 			- Custom Defined Smart Pointers 
-    // -------------------------------------------
-
-/*#[derive(Debug)]
-enum List {
-    Cons(i32, Box<List>),
-    Nil,
-}
-
-fn main() {
-    let list = List::Cons(1, Box::new(List::Cons(2, Box::new(List::Cons(3, Box::new(List::Nil))))));
-    println!("{:?}",list);
-}
-*/
-
-//Example #2: 
-/*
-enum List {
-   Cons(i32, Option<Box<List>>),    
- }
- 
-use List::{Cons};
-
-fn main() {  
-   let list = List::Cons(1, Some(Box::new(List::Cons(2, Some(Box::new(List::Cons(3, None)))))));
-}
-
-*/ 
-
-
-
-/* 
-struct MySmartPointer{value: i32}    // add it at the end 
-    impl MySmartPointer {
-        fn new(x:i32)-> MySmartPointer{
-           MySmartPointer{value: x}
-    
-        }
-    }
-    
-   use std::ops::Deref; 
-   /*
-   trait Deref {
-    type Target: name_of_type;
-    fn deref(&self) -> &Self::Target;
-   }  
-   */ 
-
-   
-   impl Deref for MySmartPointer {
-   type Target = i32;
-   fn deref(&self) -> &i32 {
-      &self.value //returns data
-      }
-   }
-   
-    /*
-    pub trait Drop {
-    fn drop(&mut self);
-   }
-    */
-    
-    impl Drop for MySmartPointer{
-        fn drop(&mut self){
-           println!("dropping MySmartPointer object from memory {:?}", self.value);
-        }
-    }
-    
-        
-     fn main() {
-        
-        let a = 50;
-        let b = &a;   //let b = Box::new(a); 
-        println!("{}", 50 == a);
-        println!("{}", 50 == *b); 
-        //println!("{}", a == b); 
-        
-          
-        let sptr1 = MySmartPointer::new(a);
-        let sptr2 = MySmartPointer::new(a+3);
-        let sptr3 = MySmartPointer::new(a+6);
-         
-        println!("{}", a==*sptr1); // *(sptr1.deref())                         // In the first place explain the deref and then in the second case explain the drop
-        let z = *sptr1; 
-
-        println!("The items will be dropped after this line executes");     
-        drop(sptr2);   
-    }
-
-*/ 
-
+// -------------------------------------------
+// 			Smart Pointers
+//          	- Box Pointers
+//          	- Issue with Box Pointer implementation of Cons variant in List
+// 			- Custom Defined Smart Pointers
+// -------------------------------------------
 
 use std::ops::Deref;
 
-struct Courses {
-    courses_list: Vec<String>,
-    department: String,
-    year: u32,
+#[derive(Debug)]
+enum List {
+    Cons(i32, Option<Box<List>>),
 }
 
-impl Deref for Courses {
-    type Target = Vec<String>;
+struct MySmartPointer {
+    value: i32,
+}
 
-    fn deref(&self) -> &Vec<String> {
-        &self.courses_list
+impl MySmartPointer {
+    // fn new(value: i32) -> MySmartPointer {
+    //     MySmartPointer { value }
+    // }
+
+    fn new(value: i32) -> Self {
+        Self { value }
+    }
+}
+
+impl Deref for MySmartPointer {
+    type Target = i32;
+    fn deref(&self) -> &Self::Target {
+        &self.value
+    }
+}
+
+impl Drop for MySmartPointer {
+    fn drop(&mut self) {
+        println!(
+            "This function runs when the variable being dropped (manually or when it goes out of scope"
+        );
     }
 }
 
 fn main() {
-    let current_courses = Courses {
-        courses_list: vec!["CS101".to_string(), "CS303".to_string(), "CS400".to_string()],
-        department: String::from("CS"),
-        year: 2022,
-    };
+    let list = List::Cons(1, Some(Box::new(List::Cons(2, None))));
+    println!("{:?}", list);
 
-    println!("{:?}", *current_courses);
+    let List::Cons(x, some) = list;
+
+    println!("the contained number is {}", x);
+    println!("the contained child list is {:?}", some);
+
+    let v = 50;
+    let mysmtpr = MySmartPointer::new(v);
+
+    // dereffing the pointer
+    let v2 = *mysmtpr;
+
+    println!("this is the dereffed value {}", v2);
 }
-
-
