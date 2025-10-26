0e32ebd1f2a422c27c73768a3061eaa81f3f98f2
jojostx <ikuskid7@gmail.com>
2025-10-26 18:26:04 +0100

    custom linkedlist

--- 9_smart_pointers_in_rust/7_implementing_singly_link_list_from_scratch.rs
+++ 9_smart_pointers_in_rust/7_implementing_singly_link_list_from_scratch.rs
@@ -1,54 +1,31 @@
 // -------------------------------------------
-// 		Link List (Part 1) 
+// 		Link List (Part 1)
 // -------------------------------------------
 
 #[derive(Debug)]
-struct linklist{
-   head: pointer, 
+struct LinkedList {
+    head: Pointer,
 }
+
 #[derive(Debug)]
-struct Node{
-    element: i32, 
-    next: pointer,     
-  }
+struct Node {
+    value: i32,
+    next: Pointer,
+}
 
-type pointer = Option<Box<Node>>; 
+type Pointer = Option<Box<Node>>;
 
-fn main(){
-   /* 
-   
-   let list = Node{element: 1, next: None};
+impl Node {
+    fn new(value: Option<i32>, pointer: Pointer) -> Option<Self> {
+        value.map(|value| Self {
+            value,
+            next: pointer,
+        })
+    }
+}
 
-   let list = Node{element: 1, next: Some(Box::new(Node {
-      element: 2, next: Some(Box::new(Node {
-         element:3, next: None
-      }))
-   }))};
+fn main() {
+    let node = Node::new(Some(1), None);
 
-   let list = linklist {head: Node{element: 1, next: None}};
-   
-   let list = linklist {head: Node { element: 1, next: Some(Box::new(Node {
-      element: 2, next: Some(Box::new(Node {
-         element: 3, next: None
-      }))
-   })) }}; 
- */ 
-
- let list = linklist{head: None};
-
- let list = linklist{head: Some(Box::new(Node {element: 100, next: (
-   Some(Box::new(Node {
-      element: 200, next: None
-   }))
- )}))}; 
-
- //println!("{:?}", list.head.unwrap().element);  
-
-
- //println!("{:?}", list.head.unwrap().next.unwrap().element);  
-
- println!("{:?}", list.head);
-
-
-
+    println!("{:?}", node);
 }
